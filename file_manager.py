89f1312c6561a1f0c17b9b1b48ab0fdde569ca45
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:13:33 +0000

    Adding numeric_owner as keyword arguement

--- file_manager.py
+++ file_manager.py
@@ -78,7 +78,7 @@ class FileManager:
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tf, destination)
